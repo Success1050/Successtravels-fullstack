9646d8cb8c94e598313949de9969618c07fe84e6
emmanuel success <emmanuelekwunife58@gmail.com>
2024-12-30 13:43:02 -0800

    another changes

--- successtravels/successtravels/settings.py
+++ successtravels/successtravels/settings.py
@@ -33,7 +33,7 @@ SECRET_KEY = config('SECRET_KEY')
 DEBUG= config('DEBUG', cast= bool)
 
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['*', 'https://successtravels-fullstack-3.onrender.com']
 
 
 # Application definition
