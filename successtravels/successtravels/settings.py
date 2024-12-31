f6d59f3c7b454031501f36c7d9b3f43f33f07609
emmanuel success <emmanuelekwunife58@gmail.com>
2024-12-31 05:53:15 -0800

    another changes

--- successtravels/successtravels/settings.py
+++ successtravels/successtravels/settings.py
@@ -88,13 +88,9 @@ WSGI_APPLICATION = 'successtravels.wsgi.application'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': os.getenv('DB_NAME'),
-        'USER': os.getenv('DB_USER'),
-        'PASSWORD': os.getenv('DB_PWD'),
-        'HOST': os.getenv('DB_HOST'),
-        'PORT': os.getenv('DB_PORT'),
+    "default": {
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": "mydatabase",
     }
 }
 
